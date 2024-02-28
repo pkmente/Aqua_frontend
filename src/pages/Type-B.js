594289d27008b11417de14f842932133347d8244
pkmente <128295053+pkmente@users.noreply.github.com>
2024-02-28 13:37:21 +0530

    Update Type-B.js

--- src/pages/Type-B.js
+++ src/pages/Type-B.js
@@ -19,7 +19,7 @@ const TypeB = ({ onClose }) => {
                 console.error(error);
             }
         };
-        const intervalId = setInterval(fetchData, 2000);
+        const intervalId = setInterval(fetchData, 500);
         return () => clearInterval(intervalId);
     }, []);
     return (
