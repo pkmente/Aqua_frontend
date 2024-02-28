9bdf013ee250c4659d3a4ca4dfc15c67b41e339a
pkmente <128295053+pkmente@users.noreply.github.com>
2024-02-28 13:26:49 +0530

    Update Type-F.js

--- src/pages/Type-F.js
+++ src/pages/Type-F.js
@@ -12,7 +12,7 @@ const TypeF = ({ onClose }) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:1111/api/sensordata");
+                const response = await fetch("https://aqua-liart-eight.vercel.app/api/sensordata");
                 const data = await response.json();
                 setTypef(data.typef);
             } catch (error) {
