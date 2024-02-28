0e383f69d53ed8de312fc56b3d143cf1ee85ff3d
pkmente <128295053+pkmente@users.noreply.github.com>
2024-02-28 13:38:24 +0530

    Update Type-E.js

--- src/pages/Type-E.js
+++ src/pages/Type-E.js
@@ -19,7 +19,7 @@ const TypeE = ({ onClose }) => {
                 console.error(error);
             }
         };
-        const intervalId = setInterval(fetchData, 2000);
+        const intervalId = setInterval(fetchData, 500);
         return () => clearInterval(intervalId);
     }, []);
     return (
