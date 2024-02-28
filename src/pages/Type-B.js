8a2ba6272b737cf4d43340f9ac27bef3431c2ccf
pkmente <128295053+pkmente@users.noreply.github.com>
2024-02-28 13:25:20 +0530

    Update Type-B.js

--- src/pages/Type-B.js
+++ src/pages/Type-B.js
@@ -12,7 +12,7 @@ const TypeB = ({ onClose }) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:1111/api/sensordata");
+                const response = await fetch("https://aqua-liart-eight.vercel.app/api/sensordata");
                 const data = await response.json();
                 setTypeb(data.typeb);
             } catch (error) {
